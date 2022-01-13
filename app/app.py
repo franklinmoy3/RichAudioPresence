3bac8bc73af2b7bb57f3b34520ff51b83ef64308
Franklin Moy <franklinmoy3@gmail.com>
2022-01-12 21:10:47 -0600

    Home view functional but needs touch-up

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ import asyncio
 from queue import Queue
 import time
 
-from secrets import app_id
+from secrets import app_id, small_image_key, large_image_key
 import discordsdk as dsdk
 import winrt_audio as wrt
 
@@ -20,20 +20,27 @@ class DiscordThread(Thread):
         self.root = root
 
     def run(self):
+        """Override of Thread.run method"""
         while True:
-            time.sleep(5/10)
+            time.sleep(5 / 10)
             self.update_activity()
             self.instance.run_callbacks()
 
     def update_activity(self):
+        """Sets the users Discord activity based on media session"""
         media_info = asyncio.run(wrt.get_media_info())
         if media_info == str("No active media"):
             self.activity_manager.clear_activity(self.callback)
             self.root.event_generate("<<UpdateEvent>>")
             return
         activity = dsdk.Activity()
-        activity.state = str(media_info['artist'])
+        activity.state = str(media_info["artist"])
         activity.details = f"Listening to {media_info['title']}"
+        activity.assets = dsdk.model.ActivityAssets()
+        activity.assets.large_image = str(large_image_key)
+        activity.assets.large_text = "Rich Audio Presence"
+        activity.assets.small_image = str(small_image_key)
+        activity.assets.small_text = "Windows OS"
         self.activity_manager.update_activity(activity, self.callback)
         self.queue.put(media_info)
         self.root.event_generate("<<UpdateEvent>>")
@@ -46,55 +53,96 @@ class DiscordThread(Thread):
             raise Exception(result)
 
 
-class GUI(tk.Frame):
+class RootSetup:
+    """Class that sets up the root tkinter window"""
+
     def __init__(self, root: tk.Tk):
-        # Tkinter window setup
-        tk.Frame.__init__(self, root)
         self.root = root
-        self.root.title("Rich Audio Presence")
 
-        self.menu_bar = tk.Menu(self.root)
-        self.file = tk.Menu(self.menu_bar, tearoff=1)
-        self.file.add_command(label="Settings")
-        self.file.add_command(label="Quit", command=self.root.quit)
-        self.help = tk.Menu(self.menu_bar, tearoff=1)
-        self.help.add_command(label="Manual")
-        self.help.add_command(label="About")
+        root.title("Rich Audio Presence")
+        root.resizable(width=False, height=False)
 
-        self.status_string = tk.StringVar(value="Initializing...")
-        self.song_string = tk.StringVar(value="None")
-        self.artist_string = tk.StringVar(value="None")
+        # Menu bar
+        menu_bar = tk.Menu(root)
+        root["menu"] = menu_bar
 
-        self.status_header = ttk.Label(self.root, text="Playback Status")
-        self.status_label = ttk.Label(self.root,
-                                      textvariable=self.status_string)
-        self.song_header = ttk.Label(self.root, text="Song")
-        self.song_label = ttk.Label(self.root, textvariable=self.song_string)
-        self.artist_header = ttk.Label(self.root, text="Artist")
-        self.artist_label = ttk.Label(self.root,
-                                      textvariable=self.artist_string)
+        file_menu = tk.Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="Settings")
+        file_menu.add_command(label="Exit", command=root.quit)
+        menu_bar.add_cascade(menu=file_menu, label="File")
 
-        self.root.bind("<<UpdateEvent>>", self.update_strings)
+        help_menu = tk.Menu(menu_bar, tearoff=0)
+        help_menu.add_command(label="Manual")
+        help_menu.add_command(label="About")
+        menu_bar.add_cascade(menu=help_menu, label="Help")
+
+        # Frame layout
+        root_frame = ttk.Frame(
+            root, borderwidth=5, relief="ridge", width=400, height=400
+        )
+        root_frame.grid(row=0, column=0)
+
+        # Frame content
+        status_header = ttk.Label(
+            root_frame, text="Playback Status", font="TkDefaultFont 16 bold"
+        )
+        self.status_string = tk.StringVar(value="No Active Media")
+        self.status_box = ttk.Entry(
+            root_frame,
+            textvariable=self.status_string,
+            state=tk.DISABLED,
+            justify=tk.CENTER,
+        )
+        status_header.grid(row=0, column=0, padx=15, pady=15)
+        self.status_box.grid(row=1, column=2, columnspan=6, padx=15, pady=15)
+
+        song_header = ttk.Label(root_frame, text="Song", font="TkDefaultFont 16 bold")
+        self.song_string = tk.StringVar()
+        self.song_box = ttk.Entry(
+            root_frame,
+            textvariable=self.song_string,
+            state=tk.DISABLED,
+            justify=tk.CENTER,
+        )
+        song_header.grid(row=2, column=0, padx=15, pady=15)
+        self.song_box.grid(row=3, column=2, columnspan=6, padx=15, pady=15)
+
+        artist_header = ttk.Label(
+            root_frame, text="Artist", font="TkDefaultFont 16 bold"
+        )
+        self.artist_string = tk.StringVar()
+        self.artist_box = ttk.Entry(
+            root_frame,
+            textvariable=self.artist_string,
+            state=tk.DISABLED,
+            justify=tk.CENTER,
+        )
+        artist_header.grid(row=4, column=0, padx=15, pady=15)
+        self.artist_box.grid(row=5, column=2, columnspan=6, padx=15, pady=15)
+
+        # Set up bindings
+        root.bind("<<UpdateEvent>>", self.update_strings)
 
         # Create shared queue and separate thread for discord updater
         self.queue = Queue()
-        discord_thread = DiscordThread(self.root, self.queue)
+        discord_thread = DiscordThread(root, self.queue)
         discord_thread.start()
 
     def update_strings(self, event):
+        """Routine to update the strings shown on the GUI"""
         if self.queue.empty():
-            self.status_string = "No active media"
-            self.song_string = ""
-            self.artist_string = ""
+            self.status_string.set("No active media")
+            self.song_string.set("")
+            self.artist_string.set("")
         else:
             media_info = self.queue.get()
-            self.status_string = "Now Playing"
-            self.song_string = media_info['title']
-            self.artist_string = media_info['artist']
+            self.status_string.set("Now Playing")
+            self.song_string.set(media_info["title"])
+            self.artist_string.set(media_info["artist"])
             self.queue.task_done()
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    GUI(root)
+    RootSetup(root)
     root.mainloop()
--- app/winrt_audio.py
+++ app/winrt_audio.py
@@ -1,5 +1,7 @@
-from winrt.windows.media.control import \
-    GlobalSystemMediaTransportControlsSessionManager as MediaManager
+from winrt.windows.media.control import (
+    GlobalSystemMediaTransportControlsSessionManager as MediaManager,
+)
+
 
 async def get_media_info():
     sessions = await MediaManager.request_async()
@@ -9,9 +11,12 @@ async def get_media_info():
         info = await current_session.try_get_media_properties_async()
 
         # song_attr[0] != '_' ignores system attributes
-        info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
+        info_dict = {
+            song_attr: info.__getattribute__(song_attr)
+            for song_attr in dir(info)
+            if song_attr[0] != "_"
+        }
         # Convert WinRT vector to list
-        info_dict['genres'] = list(info_dict['genres'])
+        info_dict["genres"] = list(info_dict["genres"])
         return info_dict
-    print("No active media")
     return str("No active media")
